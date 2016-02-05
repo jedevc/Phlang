408305d56883597e26c25f961d2fb9b8baab3b9f
Justin Chadwell <jedevc@gmail.com>
2016-02-05 18:26:36 +0545

    Added ptimer for push timers.

--- source/code.rb
+++ source/code.rb
@@ -136,8 +136,52 @@ class TimerTrigger < Trigger
     end
 end
 
+class PushTimerTrigger < Trigger
+    def initialize(args)
+        super(args)
+        @ending = {}
+    end
+
+    public
+    def add(bot, response)
+        bot.add_handle("send-event", lambda do |m, r|
+            reg = Regexp.new(@args.slice(1, @args.length).join).match(m["content"])
+            if reg
+                if @ending.include?(r)
+                    push_time(r, @args[0].to_i)
+                else
+                    add_time(r, @args[0].to_i) do
+                        response.call(reg, m, r)
+                    end
+                end
+            end
+            return false
+        end)
+    end
+
+    private
+    def add_time(room, delay, &blk)
+        @ending[room] = Time.now + delay
+        room.intime(delay, lambda do
+            if @ending[room] < Time.now
+                blk.call()
+                @ending.delete(room)
+            else
+                add_time(room, @ending[room] - Time.now, &blk)
+            end
+        end)
+    end
+
+    def push_time(room, delay)
+        if @ending.include?(room)
+            @ending[room] = Time.now + delay
+        end
+    end
+end
+
 TRIGGERS = {"msg" => lambda do |args| return MessageTrigger.new(args) end,
-            "timer" => lambda do |args| return TimerTrigger.new(args) end
+            "timer" => lambda do |args| return TimerTrigger.new(args) end,
+            "ptimer" => lambda do |args| return PushTimerTrigger.new(args) end,
 }
 
 class Block
