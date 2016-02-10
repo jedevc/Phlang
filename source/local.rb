233ec2ef021e377856bb408795f2bac884dc71c0
Justin Chadwell <jedevc@gmail.com>
2016-02-10 16:47:02 +0545

    Improved file loading.

--- source/local.rb
+++ source/local.rb
@@ -1,26 +1,35 @@
 require_relative 'phlangbot'
 require_relative 'config'
 
-def load_bots(place, options, extension="phlang")
-    base = File.join(Dir.pwd, place)
-
+def load_source(place, subs, extension="phlang")
     sources = {}
 
-    options.each do |opt|
-        target = File.join(base, opt, "*.#{extension}")
-        Dir.glob(target) do |filename|
-            File.open(filename, 'r') do |file|
-                source = file.read
-                sources[File.basename(filename, ".#{extension}")] = source
+    subs.each do |s|
+        Dir.glob(File.join(place, s + ".#{extension}")) do |filename|
+            if File.file?(filename)
+                File.open(filename, 'r') do |file|
+                    source = file.read
+                    sources[File.basename(filename, ".#{extension}")] = source
+                end
+            end
+        end
+
+        Dir.glob(File.join(place, s)) do |filename|
+            if File.directory?(filename)
+                sources.merge!(load_source(filename, ["*"], extension))
             end
         end
     end
 
+    return sources
+end
+
+def load_bots(source)
     conf = PhlangBotConfig.new(MINIMAL_BUILTINS, FULL_TRIGGERS, FULL_RESPONSES)
 
     bots = []
-    sources.each_key do |k|
-        b = PhlangBot.new(k, sources[k], conf)
+    source.each_key do |k|
+        b = PhlangBot.new(k, source[k], conf)
         bots.push(b)
     end
     return bots
--- source/main.rb
+++ source/main.rb
@@ -13,14 +13,15 @@ def main(opts)
     begin
         bs = BotGroup.new()
 
-        base, options = "bots", opts[:where]
-        loaded = load_bots(base, options)
+        base, options = "bots", opts[:file]
+        source = load_source(base, options)
+        loaded = load_bots(source)
         if loaded.length > 0
             loaded.each do |b|
                 bs.add(b)
             end
         else
-            LogService.get.fatal "could not find any bots in (#{base} #{options})"
+            LogService.get.fatal "could not find any bots in (#{base}/#{options})"
             return
         end
 
@@ -38,14 +39,14 @@ end
 
 if __FILE__ == $0
     # Set default args
-    options = {:where => [""], :room => "costofcivilization", :logging => STDOUT}
+    options = {:file => [], :room => "costofcivilization", :logging => STDOUT}
 
     # Parse command line args
     OptionParser.new do |opts|
         opts.banner = "Usage: ./run_phlang.sh [options]"
 
-        opts.on("-wWHERE", "--where=WHERE", "Folder in bots/ to load from") do |v|
-            options[:where].push(v)
+        opts.on("-fFILE", "--file=FILE", "File in bots/ to load from") do |v|
+            options[:file].push(v)
         end
 
         opts.on("-rROOM", "--room=ROOM", "Room to spawn bots in") do |v|
