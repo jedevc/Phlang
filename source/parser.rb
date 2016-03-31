d9f2cf71d3cb3ede998587c4cc94208691a968d5
Justin Chadwell <jedevc@gmail.com>
2016-03-31 10:25:24 +0700

    Catch a couple of nasty parsing errors.

--- source/parser.rb
+++ source/parser.rb
@@ -15,31 +15,33 @@ class Parser
         blocks = []
 
         trigger = nil
-        response = []
+        responses = []
 
         @bits.each do |bit|
             if bit == "end"
                 # Create block from previously collected data
-                block = Block.new()
-                block.add_trigger(trigger[0], trigger.slice(1, trigger.length))
-                response.each do |resp|
-                    block.add_response(resp[0], resp.slice(1, resp.length))
+                if !trigger.nil? and responses.length > 0
+                    block = Block.new()
+                    block.add_trigger(trigger[0], trigger.slice(1, trigger.length))
+                    responses.each do |resp|
+                        block.add_response(resp[0], resp.slice(1, resp.length))
+                    end
+                    blocks.push(block)
                 end
-                blocks.push(block)
 
                 # Reset vars
                 trigger = nil
-                response = []
+                responses = []
             elsif @allowed_triggers.include?(bit)
                 # New trigger
                 trigger = [bit]
             elsif @allowed_responses.include?(bit)
                 # New response
-                response.push([bit])
+                responses.push([bit])
             else
                 # Arg for trigger/response
-                if response.length > 0
-                    response[-1].push(bit)
+                if responses.length > 0
+                    responses[-1].push(bit)
                 elsif trigger.length > 0
                     trigger.push(bit)
                 end
