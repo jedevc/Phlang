18ad55158ce11d0052d4c3b84c2a30ebb484625b
Justin Chadwell <jedevc@gmail.com>
2016-03-31 11:24:34 +0700

    Manage option parser failure better.

--- source/main.rb
+++ source/main.rb
@@ -5,6 +5,7 @@ require_relative 'logservice'
 
 require 'optparse'
 
+# Crash program after error in other threads
 Thread.abort_on_exception = true
 
 def main(opts)
@@ -49,37 +50,42 @@ if __FILE__ == $0
     }
 
     # Parse command line args
-    OptionParser.new do |opts|
-        opts.banner = "Usage: ./run_phlang.sh [options]"
+    begin
+        OptionParser.new do |opts|
+            opts.banner = "Usage: ./run_phlang.sh [options]"
 
-        opts.on("-f FILE", "--file FILE", "Where to load bots from") do |v|
-            options[:file].push(v)
-        end
-
-        opts.on("-r ROOM", "--room ROOM", "Room to spawn bots in") do |v|
-            options[:room] = v
-        end
-
-        opts.on("-p PASSWORD", "--password PASSWORD", "Password for the room") do |v|
-            options[:password] = v
-        end
-
-        opts.on("-l LOGFILE", "--log LOGFILE", "File to output logs to") do |v|
-            options[:logging] = v
-        end
-
-        opts.on("-c CREATOR", "--creator CREATOR", "Who the bot should say their creator is") do |v|
-            options[:creator] = v
-        end
-
-        opts.on("-s SECURITY", "--security SECURITY", "Security setting to use") do |v|
-            if v == "low"
-                options[:config] = LOW_SECURITY
-            elsif v == "high"
-                options[:config] = HIGH_SECURITY
+            opts.on("-f FILE", "--file FILE", "Where to load bots from") do |v|
+                options[:file].push(v)
             end
-        end
-    end.parse!
+
+            opts.on("-r ROOM", "--room ROOM", "Room to spawn bots in") do |v|
+                options[:room] = v
+            end
+
+            opts.on("-p PASSWORD", "--password PASSWORD", "Password for the room") do |v|
+                options[:password] = v
+            end
+
+            opts.on("-l LOGFILE", "--log LOGFILE", "File to output logs to") do |v|
+                options[:logging] = v
+            end
+
+            opts.on("-c CREATOR", "--creator CREATOR", "Who the bot should say their creator is") do |v|
+                options[:creator] = v
+            end
+
+            opts.on("-s SECURITY", "--security SECURITY", "Security setting to use") do |v|
+                if v == "low"
+                    options[:config] = LOW_SECURITY
+                elsif v == "high"
+                    options[:config] = HIGH_SECURITY
+                end
+            end
+        end.parse!
+    rescue OptionParser::InvalidOption => e
+        puts e
+        abort
+    end
 
     # Call main function
     main(options)
