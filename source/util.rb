7e49395da715912f22585fc0d01acd37db852b81
Justin Chadwell <jedevc@gmail.com>
2016-04-23 22:19:24 +0545

    Fix to_number to work with numbers.

--- source/util.rb
+++ source/util.rb
@@ -1,7 +1,11 @@
 def to_number(ns)
-    if ns.include? '.'
-        return ns.to_f
+    if ns.class == String
+        if ns.include? '.'
+            return ns.to_f
+        else
+            return ns.to_i
+        end
     else
-        return ns.to_i
+        return ns
     end
 end
